21a0e7a3fcea141378e7942cfcfb65e30b37ac1e
Gregor Wolf <gregor.wolf@gmail.com>
2014-12-22 18:30:44 +0100

    Extract function to read basic authentication data

--- server-basic-auth.js
+++ server-basic-auth.js
@@ -20,18 +20,24 @@ var basic = auth.basic({
     }
 );
 
+function getBasicAuthData(req){
+  var header = req.headers['authorization']||'',        // get the header
+      token   = header.split(/\s+/).pop()||'',            // and the encoded auth token
+      auth    = new Buffer(token, 'base64').toString(),    // convert from base64
+      parts   = auth.split(/:/),                          // split on colon
+      data    = {};
+  data['username'] = parts[0];
+  data['password'] = parts[1];
+  return data;
+}
+
 // Creating new HTTP server.
 http
   .createServer(basic, function(req, res) {
-    var header=req.headers['authorization']||'',        // get the header
-        token=header.split(/\s+/).pop()||'',            // and the encoded auth token
-        auth=new Buffer(token, 'base64').toString(),    // convert from base64
-        parts=auth.split(/:/),                          // split on colon
-        username=parts[0],
-        password=parts[1];
+    var data = getBasicAuthData(req);
 
     res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end('username is "'+username+'" and password is "'+password+'"');
+    res.end('username is "' + data.username + '" and password is "' + data.password + '"');
   })
   .listen(port, function () {
     console.log('SAP HANA Cloud Tial Authentication Proxy for HANA XS Services ready: http://localhost:' + port);
